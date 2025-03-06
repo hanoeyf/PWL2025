98f896a7c0544ca1dfd934e215e254973e2e5164
NamaAnda <hanifahkurniasari0512@gmail.com>
2025-03-07 00:11:05 +0700

    jobsheet02_ROUTING, CONTROLLER, DAN VIEW

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,14 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Http\Request;
-class WelcomeController extends Controller
-{
-    public function hello() {
-        return 'Hello World';
-        }
-        
-}
 
 class Controller extends BaseController
 {
--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,29 @@
 <?php
-
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PageController;
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-Route::get(‘/hello’, [WelcomeController::class,’hello’]);
+use App\Http\Controllers\PhotoController;
+use App\Http\Controllers\WelcomeController;
+
+// 
+Route::resource('photos', PhotoController::class);
+
+Route::get('/hello', [WelcomeController::class,'hello']);
 
 Route::get('/', [PageController::class, 'index']);
 Route::get('/about', [PageController::class, 'about']);
 Route::get('/articles/{id}', [PageController::class, 'articles']);
 
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/about', [AboutController::class, 'show']);
+Route::get('/articles/{id}', [ArticleController::class, 'show']);
 
+Route::get('/greeting', function () {
+    return view('blog.hello', ['name' => 'Hanifah']);
+    });
 
+Route::get('/greeting', [WelcomeController::class, 'greeting']);
     
 
 
